f27bd8cc2ca54e464eabaca54624ed017002a686
kamuie <kamuie>
2021-08-08 02:03:26 -0300

    ++

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 async function bootstrap() {
 
 
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {cors: true});
     const config = new DocumentBuilder()
         .setTitle('BIP')
         .setDescription('BIP api')
