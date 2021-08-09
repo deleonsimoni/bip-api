b789e31a55fd70b036eca53e52416c80a86ab8e5
Deleon Simoni <deleon.simoni@gmail.com>
2021-08-09 00:41:05 -0300

    alteracao nome modulos

--- src/domain/section/section.module.ts
+++ src/domain/section/section.module.ts
@@ -2,7 +2,7 @@ import {Module} from '@nestjs/common';
 import {SectionService} from './section.service';
 import {SectionController} from './section.controller';
 import {MongooseModule} from "@nestjs/mongoose";
-import {Section, SectionSchema} from "../schemas/Section";
+import {Section, SectionSchema} from "../schemas/section";
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Section.name, schema: SectionSchema }])],
