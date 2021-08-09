46ce6c90048a3adad9f963295b4663299a17c5c2
kamuie <kamuie>
2021-08-08 22:50:48 -0300

    ++

--- src/domain/generics/generic.controller.ts
+++ src/domain/generics/generic.controller.ts
@@ -11,6 +11,8 @@ export class GenericController<T extends Document> {
 
     @Post()
     async create(@Body() obj, @Request() req) {
+        //TODO remover gambiarra password quando implementar envio de emil
+        obj.password = "123456";
         obj.owner = req.user.id;
         return this.service.create(obj);
     }
